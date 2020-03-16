2f5f387f5886bbbe8fa8ce50302e283712494aca
gnshkhr <53820653+gnshkhr@users.noreply.github.com>
2020-03-16 21:06:14 +0530

    Add files via upload
    1.make folder ajax to save ajax.js file
    2.make another folder to save database.php & save.php file

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ save.php
@@ -0,0 +1,64 @@
+<?php
+include 'database.php';
+if(count($_POST)>0){
+	if($_POST['type']==1){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$city=$_POST['city'];
+		$sql = "INSERT INTO `crud`( `name`, `email`,`phone`,`city`) 
+		VALUES ('$name','$email','$phone','$city')";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==2){
+		$id=$_POST['id'];
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$city=$_POST['city'];
+		$sql = "UPDATE `crud` SET `name`='$name',`email`='$email',`phone`='$phone',`city`='$city' WHERE id=$id";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("statusCode"=>200));
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==3){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM `crud` WHERE id=$id ";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+if(count($_POST)>0){
+	if($_POST['type']==4){
+		$id=$_POST['id'];
+		$sql = "DELETE FROM crud WHERE id in ($id)";
+		if (mysqli_query($conn, $sql)) {
+			echo $id;
+		} 
+		else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		mysqli_close($conn);
+	}
+}
+
+?>
